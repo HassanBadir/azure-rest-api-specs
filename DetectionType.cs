94ec488d30a0d039a53b2ddc2b7c43f071cb170d
HassanBadir <37299319+HassanBadir@users.noreply.github.com>
2019-03-04 09:50:33 +0200

    Add files via upload

---
+++ DetectionType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Commands.Sql.ThreatDetection.Model
+{
+    public static class DetectionType
+    {
+        public const string Sql_Injection = "Sql_Injection";
+        public const string Sql_Injection_Vulnerability = "Sql_Injection_Vulnerability";
+        public const string Access_Anomaly = "Access_Anomaly";
+        public const string Data_Exfiltration = "Data_Exfiltration";
+        public const string Unsafe_Action = "Unsafe_Action";
+        public const string None = "None";
+    }
+}
